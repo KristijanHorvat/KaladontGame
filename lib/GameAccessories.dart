5e482e3b42746f59a0319e23368195c260c1d3eb
KristijanHorvat <kristijanvtc@gmail.com>
2022-10-19 00:28:39 +0200

    nj dž lj bi trebali raditi treba još testirati, ali ugrađeno je.

--- lib/GameAccessories.dart
+++ lib/GameAccessories.dart
@@ -4,6 +4,8 @@ import 'dart:math';
 import 'package:first_app/allWords200K.dart';
 
 //List<String> arrayOfWords = ['argument', 'kaladont', 'auto', 'brod', 'komp', 'cement', 'bolesnik', 'kuhar', 'laptop', 'škola', 'miš', 'okej'];
+//List<String> arrayOfWords = ['ljekarna', "meljo", "maslac", 'kenjo', 'pedžo', 'sero', 'rogonja', 'našice', 'ljopata', 'džopa', 'njopa', 'pas'];
+
 List<String> arrayOfWords = loadWords();
 List<String> usedWords = [];
 List<String> UserTypedWords = [];
@@ -37,13 +39,19 @@ List shuffle(List items) {
   return items;
 }
 
-String getAnother(String wordPlayer){
+String getAnother(String wordPlayer){ //get new word to continue playing
   for(var newWord in shuffledWords){
     if(EndGameWords.contains(wordPlayer)==false){
       if(arrayOfWords.contains(wordPlayer)==true) {
-        if(newWord.substring(0,2) == wordPlayer.substring(wordPlayer.length-2) && usedWords.contains(newWord)==false){
+        if(wordPlayer.substring(wordPlayer.length-3, wordPlayer.length-1)=="lj" || wordPlayer.substring(wordPlayer.length-3, wordPlayer.length-1)=="nj" || wordPlayer.substring(wordPlayer.length-3, wordPlayer.length-1)=="dž"){
+          if(newWord.substring(0,3) == wordPlayer.substring(wordPlayer.length-3) && usedWords.contains(newWord)==false){
             return newWord;
           }
+        }else {
+          if(newWord.substring(0,2) == wordPlayer.substring(wordPlayer.length-2) && usedWords.contains(newWord)==false) {
+            return newWord;
+          }
+        }
         }
       else{
         return "Riječ ne postoji!";
@@ -53,10 +61,16 @@ String getAnother(String wordPlayer){
   return 'Pobjedio si!';
 }
 
-bool check(String wordComputer, String wordPlayer){
-  if(wordComputer.substring(wordComputer.length-2) == wordPlayer.substring(0,2) && wordPlayer.length>2)   {
-    return true;
-  }
+bool check(String wordComputer, String wordPlayer){ //check what user entered
+  if(wordComputer.substring(wordComputer.length-3, wordComputer.length-1)=="lj" || wordComputer.substring(wordComputer.length-3, wordComputer.length-1)=="nj" || wordComputer.substring(wordComputer.length-3, wordComputer.length-1)=="dž"){
+    if(wordComputer.substring(wordComputer.length-3) == wordPlayer.substring(0,3) && wordPlayer.length>2)   {
+      return true;
+      }
+    }else{
+      if(wordComputer.substring(wordComputer.length-2) == wordPlayer.substring(0,2) && wordPlayer.length>2)   {
+        return true;
+      }
+    }
   return false;
 }
 
